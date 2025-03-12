2f11ec3a9d3a7d4265b050f6f6d1cfadb19303f1
Lipe-lisboa <filipelisboaalves@gmail.com>
2025-03-12 11:12:02 -0300

    Comentario  index.py

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ from baixar_arquivo_zip import baixar_arquivo
 from extrair_csv import extrair
 from dividir_converter_csv_mes import datas_menssais, dividir_csv_por_mes
 
+# Juntando todas as etapas
+
 url_do_arquivo = 'https://www.anatel.gov.br/dadosabertos/paineis_de_dados/certificacao_de_produtos/produtos_certificados.zip' # url do arquivo zip
 nome_do_arquivo_local = 'produtos_certificados.zip' # nome para o arquivo
 
