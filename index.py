c1124540780d24a463413a938ed39d62548ad148
Lipe-lisboa <filipelisboaalves@gmail.com>
2025-03-12 16:17:58 -0300

    Criando um scripts que pega os argumentos da linha de comando, para poder inserir na função 'dividir_csv_por_mes'

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 from baixar_arquivo_zip import baixar_arquivo
 from extrair_csv import extrair
-from dividir_converter_csv_mes import datas_menssais, convert_parquet, dividir_csv_por_mes
+from dividir_converter_csv_mes import dividir_csv_por_mes
+import sys
+
 
 # Juntando todas as etapas
-
 url_do_arquivo = 'https://www.anatel.gov.br/dadosabertos/paineis_de_dados/certificacao_de_produtos/produtos_certificados.zip' # url do arquivo zip
 nome_do_arquivo_local = 'produtos_certificados.zip' # nome para o arquivo
 
@@ -12,4 +13,10 @@ baixar_arquivo(url_do_arquivo, nome_do_arquivo_local)
 zip_path = "produtos_certificados.zip"  # Caminho do arquivo ZIP
 extrair(zip_path)
 
-dividir_csv_por_mes(2025)
+
+#Argumentos da linha de comando: python .\index.py 2025 2024 2023 ...
+print("Argumentos:", sys.argv[1:])
+
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        dividir_csv_por_mes(int(arg))
