61ef0f0af298c8267efa553e1aea1131f5c3efa7
Lipe-lisboa <filipelisboaalves@gmail.com>
2025-03-21 15:43:00 -0300

    testando a função certificados_ocds

--- PySpark.py
+++ PySpark.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession as SS, functions as F
+from teste import ocds
 
 
 spark = SS.builder.appName( "Projeto" ).getOrCreate() 
@@ -9,4 +10,35 @@ coluna = "Certificado de Conformidade Técnica"
 df_filtrado = df.filter(F.col(coluna).contains("MODERNA")).select(coluna)
 
 quantidade_certificados = df_filtrado.distinct().count()
-print('certificados:',quantidade_certificados)
+#print('certificados:',quantidade_certificados)
+
+
+def certificados_ocds(ano:int, mes:str):
+    try:
+        df = spark.read.parquet(f'arquivos_parquet/{str(ano)}/certificados_de_{mes}.parquet')
+        
+    except FileNotFoundError:
+        return "arquivo não encontrado"
+
+    coluna = "Certificado de Conformidade Técnica"
+
+    lista_ocd = ocds(ano, mes) 
+
+    saida = []
+
+    for ocd in lista_ocd:
+        df_filtrado = df.filter(F.col(coluna).contains(ocd)).select(coluna)
+        quantidade_certificados = df_filtrado.distinct().count()
+        
+        saida.append({
+            'ocd':ocd,
+            'quantidade_de_certificado':quantidade_certificados
+        })
+    
+    return saida
+
+
+certificados = certificados_ocds(2024, 'janeiro')
+print(certificados)
+for c in certificados:
+    print(c)
