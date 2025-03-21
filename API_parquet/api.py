e6a2213e42f4005b9a403855bdc99b3de4c5bd3b
Lipe-lisboa <filipelisboaalves@gmail.com>
2025-03-21 15:39:06 -0300

    criando a função certificados_ocds

--- API_parquet/api.py
+++ API_parquet/api.py
@@ -1,52 +1,40 @@
 from fastapi import FastAPI
 from pyspark.sql import SparkSession as SS, functions as F
+import pandas as pd
+import regex
 
+def ocds(ano, mes):
+    try:
+        df = pd.read_parquet(f'../arquivos_parquet/{ano}/certificados_de_{mes}.parquet')
+        
+    except FileNotFoundError:
+        print("arquivo não encontrado")
+        return
+    
+    list_ocds = []
+
+    #adiciona itens em list_ocds e list_certificado
+    for certificado in df['Certificado de Conformidade Técnica']:
+        
+        #Deixa apenas letras e traço
+        ocd = regex.sub(r'[^a-zA-Z\p{L}-_/ ]+', '', certificado).strip('/- ')
+        
+
+        #Adiciona o ocd na lista de ocds
+        if ocd not in list_ocds:
+            if ocd != '':
+                list_ocds.append(ocd)
+
+    return list_ocds
 
 # uvicorn api:app --reload
 app = FastAPI()
-
-@app.get("/")
-def hello_world_root():
-    return {"Hello": "World"}
-
-menu = [
-    {   'id': 1,
-        'name': 'coffee',
-        'price': 2.5
-     },
-    {
-        'id': 2,
-        'name': 'cake',
-        'price': 10
-    },
-    {
-        'id': 3,
-        'name': 'tea',
-        'price': 3.2
-    },
-    {
-        'id': 4,
-        'name': 'croissant',
-        'price': 5.79
-    }
-]
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int):
-
-    search = list(filter(lambda x: x["id"] == item_id, menu))
-
-    if search == []:
-        return {'Error': 'Item does not exist'}
-
-    return {'Item': search[0]}
-
 
 @app.get("/certificados/{ocd_enviado}")
 def certificados_ocd(ocd_enviado: str,ano:int, mes:str):
     spark = SS.builder.appName( "Projeto" ).getOrCreate()
     try:
-        df = spark.read.parquet(f'../arquivos_parquet/{str(ano)}/certificados_de_{mes}.parquet')
+        df = spark.read.parquet(f'../arquivos_parquet/{str(ano)}/certificados_de_{mes.lower()}.parquet')
         
     except FileNotFoundError:
         return "arquivo não encontrado"
@@ -68,3 +56,31 @@ def certificados_ocd(ocd_enviado: str,ano:int, mes:str):
     
     
     return saida
+
+
+@app.get("/certificados/{mes}/{ano}")
+def certificados_ocds(ano:int, mes:str):
+    spark = SS.builder.appName( "Projeto" ).getOrCreate()
+    try:
+        df = spark.read.parquet(f'../arquivos_parquet/{str(ano)}/certificados_de_{mes.lower()}.parquet')
+        
+    except FileNotFoundError:
+        return "arquivo não encontrado"
+
+    coluna = "Certificado de Conformidade Técnica"
+
+    lista_ocd = ocds(ano,mes) 
+
+    saida = []
+
+    if lista_ocd:
+        for ocd in lista_ocd:
+            df_filtrado = df.filter(F.col(coluna).contains(ocd)).select(coluna)
+            quantidade_certificados = df_filtrado.distinct().count()
+            
+            saida.append({
+                'ocd':ocd,
+                'quantidade_de_certificado':quantidade_certificados
+            })
+        
+        return saida
