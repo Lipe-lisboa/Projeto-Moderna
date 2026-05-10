2117e9930e2b1bc79316698cbb7f078c7860e27d
Lipe-lisboa <filipelisboaalves@gmail.com>
2026-05-10 19:55:28 -0300

    Ligando o front com o back na seção de criar os arquivos parquets

--- frontend/pages/gerenciar_dados_parquet.py
+++ frontend/pages/gerenciar_dados_parquet.py
@@ -1,23 +1,18 @@
 import streamlit as st
-from utils import API_URL, LISTA_MESES
+from utils import API_URL
+import requests
 
-st.header("Consulta Individual")
+st.header("Carregar Dados Parquet")
 
 ano = st.number_input("Ano", value=2026,max_value=2026 ,key="ano_p")
-periodo_expecifico = st.checkbox("Consultar período específico", key="periodo_expecifico")
-
-if periodo_expecifico:
-    mes_inicial = st.selectbox(
-        "Mês Inicial",
-        LISTA_MESES,
-        key="mes_inicial"
-    )
-
-    mes_final = st.selectbox(
-        "Mês Final",
-        LISTA_MESES,
-        key="mes_final"
-    )
 
 if st.button("Carregar Dados Parquet"):
-    ...
+    url_completa = f"{API_URL}/criar-parquets?ano={ano}"
+
+    with st.spinner("Buscando dados na API..."):
+        response = requests.get(url_completa)
+
+    if response.status_code == 200:
+        st.success(f"Dados de {ano} carregados!")
+    else:
+        st.error(f"Erro na API: {response.status_code}")
