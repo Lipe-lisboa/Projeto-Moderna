d68d447c98a0c59efd1e6610920383074f24e2b3
Lipe-lisboa <filipelisboaalves@gmail.com>
2026-05-10 19:28:39 -0300

    Ligando o front com o back. Além disso, criamos um campo para adicionar o resultado da requisição

--- frontend/pages/consulta_por_ocd.py
+++ frontend/pages/consulta_por_ocd.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from utils import API_URL, LISTA_MESES
+import requests
+import pandas as pd
+
 
 st.header("Consulta Individual")
 
@@ -11,4 +14,33 @@ mes_o = st.selectbox(
     key="mes_o")
 
 if st.button("Buscar OCD"):
-    ...
+    # 1. Monta a URL com os parâmetros que o usuário escolheu
+    url_completa = f"{API_URL}/certificados/{ocd_nome.upper()}?ano={ano_o}&mes={mes_o}"
+
+    with st.spinner("Buscando dados na API..."):
+        try:
+            # 2. Faz a requisição para o Backend
+            response = requests.get(url_completa)
+            
+            if response.status_code == 200:
+                dados = response.json()["result"]
+                df = pd.DataFrame(dados)
+
+                if not df.empty:
+                    total = df.iloc[0]["quantidade_de_certificado"]
+                    
+                    # 4. Exibe os resultados de forma bonita
+                    st.success(f"Dados encontrados para OCD: {ocd_nome.upper()} encontrados!")
+
+                    st.write("---")
+                    st.subheader("Total de Certificados Emitidos")
+                    st.table(df) # st.table é melhor que st.dataframe para poucas linhas
+
+                else:
+                    st.warning(f"Nenhum dado encontrado para OCD: {ocd_nome.upper()} no período selecionado.")
+                
+            else:
+                st.error(f"Erro na API: {response.status_code}")
+        
+        except Exception as e:
+            st.error(f"O Backend está desligado? Erro: {e}")
